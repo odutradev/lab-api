b356153dd5c860cd6aa0c3a481fb48a446b3add6
dutra-pc <joao.vitornl@gmail.com>
2024-11-16 20:36:09 -0300

    :sparkles: feat: Add create account finance

--- src/resources/finance/finance.service.js
+++ src/resources/finance/finance.service.js
@@ -1,9 +1,19 @@
 import accountModel from "../../models/account.js";
 
 export default class Service {
-    async createAccount(){
+    async createAccount({ name, description }){
         try {
+            const hasAccount = await accountModel.findOne({ name });
+            if (hasAccount) return { error: "account_already_exists" };
 
+            const account = new accountModel({
+                description,
+                name
+            });
+
+            await account.save();
+
+            return account;
         } catch (err) {
             return { error: "internal_error" };
         }
